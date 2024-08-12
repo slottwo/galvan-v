d73990dad22eeca4463ad02bbcb012be3c6bd9b6
slottwo <41028091+slottwo@users.noreply.github.com>
2024-08-12 14:48:47 -0300

    Clock module

--- galvan-v/main.py
+++ galvan-v/main.py
@@ -1,4 +1,15 @@
 from myhdl import *
 
+
+@block
+def clock(clk):
+
+    @always(delay(10))  # 10 nano seconds
+    def _clock():
+        clk.next = not clk
+
+    return _clock
+
+
 if __name__ == "__main__":
     pass
