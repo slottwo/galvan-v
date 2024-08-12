9767b827e114f65d2b65066a088bcb6c47fc5b81
slottwo <41028091+slottwo@users.noreply.github.com>
2024-08-12 15:22:22 -0300

    Mux modules

--- galvan-v/main.py
+++ galvan-v/main.py
@@ -11,5 +11,49 @@ def clock(clk):
     return _clock
 
 
+@block
+# Program Counter Selection Mux
+def pc_mux(reset, pc, addr, jmp, select: bool):
+
+    @always_comb
+    def mux():
+        if reset.next == ...:  # TODO: "INACTIVE_HIGH"
+            if select:
+                pc.next = jmp
+            else:
+                pc.next = addr
+
+    return mux
+
+
+@block
+# Write Data Selection Mux
+def write_data_mux(reset, write_data, mem_to_reg: bool, result, read_data):
+
+    @always_comb
+    def mux():
+        if reset.next == ...:  # TODO: "INACTIVE_HIGH"
+            if mem_to_reg:
+                write_data.next = read_data
+            else:
+                write_data.next = result
+
+    return mux
+
+
+@block
+def alu_mux(reset, im_gen, rdb, rdx, alu_src):
+
+    @always_comb
+    def mux():
+        if reset.next == ...:  # TODO: "INACTIVE_HIGH"
+            if alu_src:
+                rdx.next = im_gen
+            else:
+                rdx.next = rdb
+
+    return mux
+
+
 if __name__ == "__main__":
     pass
